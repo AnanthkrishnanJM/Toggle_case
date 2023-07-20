ae07808bf7963c5fdf9e7c23353fd85bf6201bc6
AnanthkrishnanJM <ananthkrishnan2101@gmail.com>
2023-07-20 09:47:10 +0530

    initial commit

---
+++ Toggle.java
@@ -0,0 +1,36 @@
+package ToggleCase;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Toggle {
+	String toggle(String str) {
+		String str1=" ";
+		for(int i=0;i<str.length();i++) 
+		{
+			str1+=(char)((int)str.charAt(i)^(1<<5));
+		}
+		return str1;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		Toggle tt=new Toggle();
+		System.out.println(tt.toggle(str));
+		
+		
+	}
+
+
+		    
+		   
+
+}
+
+
+
+	
+		
+	
+	
